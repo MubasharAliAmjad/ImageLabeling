f4a4d36caedf84f532f2ce5dbc7ba3331ee094f0
Sadeeed <sadeeedw@gmail.com>
2023-12-15 12:56:28 +0000

    feat(saml): update settings and move urls

--- API/urls.py
+++ API/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('saml_response/', views.SAMLResponseView.as_view(), name="saml_response"),
     # path('slice/', views.CustomSliceView.as_view()),
     path('export_data/<int:id>/', views.ExportDataview.as_view()),
+    path('saml2/', include('djangosaml2.urls'), name="saml2_login"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('API.urls')),
-    path('saml2/', include('djangosaml2.urls'), name="saml2_login"),
+    # path('saml2/', include('djangosaml2.urls'), name="saml2_login"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
