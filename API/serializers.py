966f3d2980b8f3fb2bf089290a693bda187ea86d
MubasharAli <MubasharAliAmjad@gmail.com>
2023-12-18 12:53:39 +0500

    Refactor: change the response of session

--- API/serializers.py
+++ API/serializers.py
@@ -309,9 +309,16 @@ class SessionUpdateSerializer(serializers.ModelSerializer):
         model = Session
         fields = ["id", "session_name", "notes", "case", "slices_data", "labels"]
 
+    def get_session_projects(self, instance):
+        
+        session_projects = instance.project_set.all()
+        # You can customize the serialization of each project here if needed
+        return session_projects[0].question
+
     def to_representation(self, instance):
-        # Assuming instance is your data
-        return {"session": [super().to_representation(instance)]}
+        representation = super().to_representation(instance)
+        representation['question'] = self.get_session_projects(instance)
+        return representation
     
 
     def update(self, instance, validated_data):
