adab75040783b4ab82e1114d7efd8183a4668906
MubasharAliAmjad <MubasharAliAmjad@gmail.com>
2024-01-03 16:36:03 +0500

    change token time to two days

--- core/settings.py
+++ core/settings.py
@@ -171,8 +171,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
 
 
