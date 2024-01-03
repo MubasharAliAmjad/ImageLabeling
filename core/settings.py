bcac1ea35a819e7c24f8531eb1c6b7f5c0f139a5
MubasharAliAmjad <MubasharAliAmjad@gmail.com>
2024-01-03 15:55:27 +0500

    change token time to one minute

--- core/settings.py
+++ core/settings.py
@@ -171,8 +171,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
 }
 
 
