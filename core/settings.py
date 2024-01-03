a2f604206dc17285d258ac4ab0a5aaa8f76fd268
MubasharAliAmjad <MubasharAliAmjad@gmail.com>
2024-01-03 16:24:46 +0500

    change token time to five minutes

--- core/settings.py
+++ core/settings.py
@@ -171,8 +171,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
 }
 
 
